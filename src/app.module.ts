6b89d5176d38d360c98aac6e53c5277409a934e6
PRIMERaouf <rbdzmain@gmail.com>
2025-07-24 22:34:55 +0100

    add ConfigModule and EnvConfigModule to app.module for global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './modules/orders/orders.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { EnvConfigModule } from './config/config.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [
+    OrdersModule,
+    EnvConfigModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      load: [configuration],
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
