fca216425001dc19bbebebed0203b3787af7d38f
PRIMERaouf <rbdzmain@gmail.com>
2025-07-24 14:30:35 +0100

    create getOrderUseCase and inject orderRepository to it

---
+++ src/modules/orders/application/use-cases/getOrder.usecase.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { Order } from '../../domain/entities/order.entity';
+import { OrderRepository } from '../../domain/repositories/order-repository';
+
+@Injectable()
+export class GetOrderUseCase {
+  constructor(private readonly orderRepository: OrderRepository) {}
+  async execute(id: number): Promise<Order> {
+    try {
+      const order = await this.orderRepository.findById(id);
+      if (!order) throw new Error(`Order with id ${id} not found`);
+      return order;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
