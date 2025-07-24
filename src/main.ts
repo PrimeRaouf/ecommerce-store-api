8962054f0dad0f9d867ec361578ec9a4c3a7fbfe
PRIMERaouf <rbdzmain@gmail.com>
2025-07-24 22:35:06 +0100

    enhance bootstrap logic to use EnvConfigService for dynamic port and environment logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { EnvConfigService } from './config/env-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(EnvConfigService);
+  const nodeEnv = configService.node.env;
+
+  const port = process.env.DEBUG_PORT
+    ? parseInt(process.env.DEBUG_PORT, 10)
+    : configService.node.port;
+
+  await app.listen(port);
+
+  Logger.log(
+    `🚀 Server is running on port ${port} in ${nodeEnv} mode`,
+    'Bootstrap',
+  );
 }
 bootstrap();
