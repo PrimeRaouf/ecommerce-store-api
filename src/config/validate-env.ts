df5a8820a6458a5c0fbf6d19c01e62891120c711
PRIMERaouf <rbdzmain@gmail.com>
2025-07-28 22:51:08 +0100

    add postgress envs to validate-env schema

--- src/config/validate-env.ts
+++ src/config/validate-env.ts
@@ -11,6 +11,12 @@ export function validateEnv(env: NodeJS.ProcessEnv) {
     REDIS_PORT: port({ default: 6379 }),
     REDIS_PASSWORD: str({ default: '' }),
 
+    DB_HOST: str(),
+    DB_PORT: port({ default: 5432 }),
+    DB_USERNAME: str(),
+    DB_PASSWORD: str(),
+    DB_DATABASE: str(),
+
     JWT_SECRET: str(),
     JWT_EXPIRES_IN: str(),
   });
