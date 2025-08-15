052d685b3601289dd0fcc74ab700aa4f19f1855a
PRIMERaouf <rbdzmain@gmail.com>
2025-08-15 19:12:37 +0100

    Refactor ProductRepository to accept CreateProductDto in save method and update related tests. Adjusted Redis and Postgres implementations to handle product creation and return appropriate results.

--- src/modules/products/domain/repositories/product-repository.ts
+++ src/modules/products/domain/repositories/product-repository.ts
@@ -1,9 +1,12 @@
 import { Result } from '../../../../core/domain/result';
 import { RepositoryError } from '../../../../core/errors/repository.error';
+import { CreateProductDto } from '../../presentation/dto/create-product.dto';
 import { Product } from '../entities/product';
 
 export abstract class ProductRepository {
-  abstract save(product: Product): Promise<Result<void, RepositoryError>>;
+  abstract save(
+    product: CreateProductDto,
+  ): Promise<Result<Product, RepositoryError>>;
   abstract update(product: Product): Promise<Result<void, RepositoryError>>;
   abstract findById(id: number): Promise<Result<Product, RepositoryError>>;
   abstract findAll(): Promise<Result<Product[], RepositoryError>>;
