605b20961ec4b25cfd5d2978a3d8e8c74137d298
Raouf Bouzerara <rbdzmain@gmail.com>
2025-11-01 23:28:00 +0100

    ES-83: Update Redis constants and schemas for inventory management
    - Renamed existing Redis constants for orders and products to follow a consistent naming convention.
    - Added new INVENTORY_REDIS constant for inventory-related caching.
    - Introduced InventoryIndexSchema to define the structure for inventory items, enhancing data organization and retrieval.

--- src/core/infrastructure/redis/constants/redis.constants.ts
+++ src/core/infrastructure/redis/constants/redis.constants.ts
@@ -4,16 +4,23 @@ interface IRedisContstant {
   IS_CACHED_FLAG: string;
   EXPIRATION: number; // 7 days
 }
-export const Order_REDIS: IRedisContstant = {
+export const ORDER_REDIS: IRedisContstant = {
   INDEX: 'order_index',
   CACHE_KEY: 'order_cache',
   IS_CACHED_FLAG: 'order_list:isCached',
   EXPIRATION: 3600 * 24 * 7, // 7 days
 };
 
-export const Product_REDIS: IRedisContstant = {
+export const PRODUCT_REDIS: IRedisContstant = {
   INDEX: 'product_index',
   CACHE_KEY: 'product_cache',
   IS_CACHED_FLAG: 'product_list:isCached',
+  EXPIRATION: 3600 * 24 * 7, // 7 days
+};
+
+export const INVENTORY_REDIS: IRedisContstant = {
+  INDEX: 'order_index',
+  CACHE_KEY: 'order_cache',
+  IS_CACHED_FLAG: 'order_list:isCached',
   EXPIRATION: 3600 * 24 * 7, // 7 days
 };
