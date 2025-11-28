45a5f77f8ce37cd9350abb7f5707864f09fe3647
Raouf Bouzerara <rbdzmain@gmail.com>
2025-11-28 11:58:32 +0100

    ES-92: implement merge cart usecase

--- src/modules/carts/presentation/controllers/merge-carts/merge-carts.controller.ts
+++ src/modules/carts/presentation/controllers/merge-carts/merge-carts.controller.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { Result } from '../../../../../core/domain/result';
+import { Result, isFailure } from '../../../../../core/domain/result';
 import { ControllerError } from '../../../../../core/errors/controller.error';
 import { ErrorFactory } from '../../../../../core/errors/error.factory';
+import { MergeCartsUseCase } from '../../../application/usecases/merge-carts/merge-carts.usecase';
+import { ICart } from '../../../domain/interfaces/cart.interface';
 
 @Injectable()
 export class MergeCartsController {
-  constructor() {}
+  constructor(private readonly mergeCartsUseCase: MergeCartsUseCase) {}
   async handle(
     guestCartId: string,
     userCartId: string,
-  ): Promise<Result<void, ControllerError>> {
+  ): Promise<Result<ICart, ControllerError>> {
     try {
-      return Result.success(undefined);
+      const result = await this.mergeCartsUseCase.execute({
+        guestCartId,
+        userCartId,
+      });
+
+      if (isFailure(result)) {
+        return ErrorFactory.ControllerError(result.error.message, result.error);
+      }
+
+      return Result.success(result.value);
     } catch (error) {
       return ErrorFactory.ControllerError('Unexpected Controller Error', error);
     }
